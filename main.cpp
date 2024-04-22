759d094a23f93ba389379a8f59a9397de9561214
Sonja Perković <soperkov@vsite.hr>
2024-04-22 19:58:20 +0200

    S_perkovic
    riješena vjezba 6

--- main.cpp
+++ main.cpp
@@ -6,23 +6,75 @@ int size_dialog::idd() const {
 }
 
 bool size_dialog::on_init_dialog(){
+	set_int(IDC_EDIT1, x_dialog);
+	set_int(IDC_EDIT2, y_dialog);
 	return true;
 }
 
 bool size_dialog::on_ok(){
+	try {
+		x_dialog = get_int(IDC_EDIT1);
+		y_dialog = get_int(IDC_EDIT2);
+	}
+	catch (...) {
+		return false;
+	}
 	return true;
 }
 
 
 void main_window::on_paint(HDC hdc){
+	RECT rect;
+	HBRUSH brush = CreateSolidBrush(my_color);
+
+	GetClientRect(*this, &rect);
+	SetMapMode(hdc, MM_ANISOTROPIC);
+	SetViewportExtEx(hdc, rect.right, rect.bottom, NULL);
+	SetWindowExtEx(hdc, x, y, NULL);
+
+	for (int i = 0; i < x; i++) {
+		for (int j = i % 2; j < y; j += 2) {
+			RECT rc = { i, j, i + 1, j + 1 };
+			FillRect(hdc, &rc, brush);
+		}
+	}
+	DeleteObject(brush);
+
 }
 
 void main_window::on_command(int id){
-	switch(id){
-		case ID_SIZE:
+	switch (id) {
+		case ID_SIZE: {
+			size_dialog size_dlg;
+			size_dlg.x_dialog = x;
+			size_dlg.y_dialog = y;
+
+			if (size_dlg.do_modal(0, *this) == IDOK) {
+				x = size_dlg.x_dialog;
+				y = size_dlg.y_dialog;
+				InvalidateRect(*this, NULL, true);
+			}
+
 			break;
-		case ID_COLOR:
+		}
+		case ID_COLOR: {
+			COLORREF custom_colors[16] = {0};
+			CHOOSECOLOR cc;
+
+			ZeroMemory(&cc, sizeof cc);
+			cc.lStructSize = sizeof cc;
+			cc.Flags = CC_RGBINIT | CC_FULLOPEN;
+			cc.lpCustColors = custom_colors;
+			cc.hwndOwner = *this;
+			cc.rgbResult = my_color;
+
+			if (ChooseColor(&cc)) {
+				my_color = cc.rgbResult;
+				InvalidateRect(*this, NULL, true);
+			}
+
 			break;
+		}
 		case ID_EXIT: 
 			DestroyWindow(*this); 
 			break;
--- main.h
+++ main.h
@@ -4,6 +4,9 @@
 #include "nwpdlg.h"
 
 class size_dialog : public vsite::nwp::dialog {
+public:
+	int x_dialog;
+	int y_dialog;
 protected:
 	int idd() const override;
 	bool on_init_dialog() override;
@@ -11,6 +14,10 @@ protected:
 };
 
 class main_window : public vsite::nwp::window {
+public:
+	int x = 5;
+	int y = 5;
+	COLORREF my_color = RGB(163, 163, 163);
 protected:
 	void on_paint(HDC hdc) override;
 	void on_command(int id) override;
